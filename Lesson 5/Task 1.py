f9726e051f681c2ffbe34561c106318f7610a8d8
BreadGitHub <113381962+BreadGitHub@users.noreply.github.com>
2022-10-27 19:31:17 +0300

    Add files via upload

---
+++ Lesson 5/Task 1.py
@@ -0,0 +1,5 @@
+N = int(input("Number:"))
+n = 1  #   <---Он должен выводить это!
+while N >= n**2:
+    print(n**2)
+    n = n+1
---
+++ Lesson 5/Task 6.py
@@ -0,0 +1,13 @@
+n = 0             # ЧИСЕЛ
+sm = 0            # СУММА
+sr = 0            # ОТВЕТ
+check = True
+while check:
+    N = int(input("Number:"))
+    if N == 0:
+        check = False
+    else:
+        sm = sm + N             
+        n = n + 1
+sr = sm/n         # <----- СУММУ ЧИСЕЛ ПОДЕЛИТЬ НА КОЛИЧЕСТВО!
+print(sr)
