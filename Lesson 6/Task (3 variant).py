7ac0e865c91985bcbae6807419259ae59e6d2777
BreadGitHub <113381962+BreadGitHub@users.noreply.github.com>
2022-11-26 20:11:04 +0300

    Update Task (3 variant).py

--- Lesson 6/Task (3 variant).py
+++ Lesson 6/Task (3 variant).py
@@ -1,14 +1,14 @@
 #Вариант 3
-print("Write the text")
-txt = input()
-text = ''
-n = 0
-for i in range(len(txt)):
-        if txt[i] == ".":
-            print(txt.replace('a', ''))
-            n += 1
+def func(txt):
+    text = ''
+    n = 0
+    for i in range(len(txt)):
+            if txt[i] == ".":
+                print(txt.replace('a', ''))
+                n += 1
 
-        else:
-            text += txt[i]
-print(n)
-print(text)
+            else:
+                text += txt[i]
+    print(n)
+    print(text)
+func(txt = input("Write the text:"))
