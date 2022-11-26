4eaca90110004632b599df93ee29cce7c12fa00d
BreadGitHub <113381962+BreadGitHub@users.noreply.github.com>
2022-11-26 20:02:12 +0300

    Update Task 7.py

--- Lesson 4/Task 7.py
+++ Lesson 4/Task 7.py
@@ -1,9 +1,9 @@
-N = input("How many numbers?:")
-N=int(N)
-sum = 0   #Вычислить сумму 1!+2!+3!+4!...N!
-k = 1   #Прошлый факториал
-for i in range(1, N+1):    #Только 1 for 
-    j = k * i  
-    sum += j     # ПАМЯТКА: 5! = 4! * 5 = 1 * 2 * 3 * 4* 5 
-    k = j
-print(sum)
+def func(N):
+    sum = 0   #Вычислить сумму 1!+2!+3!+4!...N!
+    k = 1   #Прошлый факториал
+    for i in range(1, N+1):    #Только 1 for 
+        j = k * i  
+        sum += j     # ПАМЯТКА: 5! = 4! * 5 = 1 * 2 * 3 * 4* 5 
+        k = j
+    print(sum)
+func(N = int(input("How many numbers?:")))
