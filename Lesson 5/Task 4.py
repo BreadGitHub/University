a449b46439e85bfd0f84c61579f5c05ae141cbfd
BreadGitHub <113381962+BreadGitHub@users.noreply.github.com>
2022-11-26 20:17:44 +0300

    Update Task 4.py

--- Lesson 5/Task 4.py
+++ Lesson 5/Task 4.py
@@ -1,10 +1,9 @@
-x = input("First Day km:")   # В первый день пробежал
-y = input("Goal:")   # Не менее у километров
-i = 1 # Дни
-x = float(x)
-y = float(y)
-while x <= y:
-    x *= 1.1
-    i += 1
-print(i)
-
+def func(x,y): 
+    i = 1 # Дни
+    x = float(x)
+    y = float(y)
+    while x <= y:
+        x *= 1.1
+        i += 1
+    print(i)
+func(x = input("First Day km:"),y = input("Goal:"))
