662e1076c35cc827238616b44c081a992cd9bf09
BreadGitHub <113381962+BreadGitHub@users.noreply.github.com>
2022-11-26 20:23:08 +0300

    Update Task 8.py

--- Lesson 5/Task 8.py
+++ Lesson 5/Task 8.py
@@ -1,18 +1,19 @@
-N = int(input("First Number:"))
-i = 0 # счетчик
-n = 1
-check = True
-while check:
-    N2 = int(input("Number:"))
-    if N2 != 0:
-        if N2 == N:
-            i += 1
+def func():
+    N = int(input("First Number:"))
+    i = 0 # счетчик
+    n = 1
+    check = True
+    while check:
+        N2 = int(input("Number:"))
+        if N2 != 0:
+            if N2 == N:
+                i += 1
+            else:
+                if i > n:
+                    i=n
+                N = N2
+                i = 0
         else:
-            if i > n:
-                i=n
-            N = N2
-            i = 0
-    else:
-        break
-print(i)
-    
+            break
+    print(i)
+func()   
