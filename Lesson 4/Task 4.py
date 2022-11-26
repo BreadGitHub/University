517a4883027cb81b20d981544bfe218f41ba8059
BreadGitHub <113381962+BreadGitHub@users.noreply.github.com>
2022-11-26 19:57:51 +0300

    Update Task 4.py

--- Lesson 4/Task 4.py
+++ Lesson 4/Task 4.py
@@ -1,8 +1,7 @@
-N = input("How many numbers?:")
-N = int(N)
-sum = 0
-for i in range(0, N):
-    sum += i
-print (sum)
-
+def func(N)
+    sum = 0
+    for i in range(0, N):
+        sum += i
+    print (sum)
+func(N = int(input("N=")))
 
