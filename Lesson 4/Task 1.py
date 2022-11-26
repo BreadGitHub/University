d367d226366cf221fe55eed6a4ab6442df141de4
BreadGitHub <113381962+BreadGitHub@users.noreply.github.com>
2022-11-26 19:52:40 +0300

    Update Task 1.py

--- Lesson 4/Task 1.py
+++ Lesson 4/Task 1.py
@@ -1,9 +1,7 @@
-A = input("Number 1:")
-B = input("Number 2:")
-A = int(A)  #Я долго до этого допирал
-B = int(B)
-for i in range(A, B+1): #Вывести от А до В, ключая B
-    if not i%2 == 0:
-        print("Uneven number:", i)  
+def func(A,B):
+    for i in range(A, B+1): #Вывести от А до В, ключая B
+        if not i%2 == 0:
+            print("Uneven number:", i)  
     
+func(A = int(input("A=")), B = int(input("B=")))   
     
