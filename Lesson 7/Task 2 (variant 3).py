80a327490efd3aa3187d90e17b159497d597efb8
BreadGitHub <113381962+BreadGitHub@users.noreply.github.com>
2022-11-26 20:25:13 +0300

    Update Task 2 (variant 3).py

--- Lesson 7/Task 2 (variant 3).py
+++ Lesson 7/Task 2 (variant 3).py
@@ -1,8 +1,10 @@
-N = [ ]
-for i in range(8):
-    print("Enter the", i, "element")
-    N.append(int(input(i)))
-    if N[i] < 15:
-        while N[i] < 15:    #Даже слишком легкое задание, я сделал его даже без исправлений
-            N[i] *= 2
-print("Array: ", N)
+def func():
+    N = [ ]
+    for i in range(8):
+        print("Enter the", i, "element")
+        N.append(int(input(i)))
+        if N[i] < 15:
+            while N[i] < 15:    #Даже слишком легкое задание, я сделал его даже без исправлений
+                N[i] *= 2
+    print("Array: ", N)
+func()
