ad5d02323ef31334ce81d8682e6c83c2c525ceb3
BreadGitHub <113381962+BreadGitHub@users.noreply.github.com>
2022-11-26 19:55:24 +0300

    Update Task 2.py

--- Lesson 4/Task 2.py
+++ Lesson 4/Task 2.py
@@ -1,16 +1,15 @@
-A = input("Number 1:")
-B = input("Number 2:")
-A = int(A)
-B = int(B)
-if A < B:
-    try:
-        for i in range(A, B + 1):
-            print(i)
-    except ValueError:
-        print("Please, write the numbers")
-else:
-    try:
-        for i in range(A, B-1, -1):
-            print(i)
-    except ValueError:
-        print("Please, write the numbers")
+def func(A,B):
+    if A < B:
+        try:
+            for i in range(A, B + 1):
+                print(i)
+        except ValueError:
+            print("Please, write the numbers")
+    else:
+        try:
+            for i in range(A, B-1, -1):
+                print(i)
+        except ValueError:
+            print("Please, write the numbers")
+
+func(A = int(input("A=")), B = int(input("B=")))
