63bc60a6962f9bd6730b38ff999c6e210a1044aa
BreadGitHub <113381962+BreadGitHub@users.noreply.github.com>
2022-11-26 19:58:49 +0300

    Update Task 5.py

--- Lesson 4/Task 5.py
+++ Lesson 4/Task 5.py
@@ -1,9 +1,9 @@
-N = input("How many namubers?:")
-N = int(N)
-sum = 0
-if N <= 0:
-    print("Nope")
-    exit()
-for i in range(0,N+1):
-    sum = i**3
-    print(sum)
+def func(N):
+    sum = 0
+    if N <= 0:
+        print("Nope")
+        exit()
+    for i in range(0,N+1):
+        sum = i**3
+        print(sum)
+func(N = input("How many namubers?:"))
