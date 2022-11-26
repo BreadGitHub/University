23d6e1508bdf73c9b2fae87da5231b0a79f8df66
BreadGitHub <113381962+BreadGitHub@users.noreply.github.com>
2022-11-26 19:56:23 +0300

    Update Task 3.py

--- Lesson 4/Task 3.py
+++ Lesson 4/Task 3.py
@@ -1,7 +1,5 @@
-A = input("Number 1:")
-B = input("Number 2:")
-A = int(A)
-B = int(B)
-#  A > B!!!
-for i in range(A%2, B-1):
-    print(i)
+def func(A,B)
+    #  A > B!!!
+    for i in range(A%2, B-1):
+        print(i)
+func(A = int(input("A=")), B = int(input("B=")))      
