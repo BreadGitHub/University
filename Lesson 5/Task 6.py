508e9efbd85b43a834d7aea5d6f7ad9d2ba47604
BreadGitHub <113381962+BreadGitHub@users.noreply.github.com>
2022-11-26 20:21:51 +0300

    Update Task 6.py

--- Lesson 5/Task 6.py
+++ Lesson 5/Task 6.py
@@ -1,13 +1,15 @@
-n = 0             # ЧИСЕЛ
-sm = 0            # СУММА
-sr = 0            # ОТВЕТ
-check = True
-while check:
-    N = int(input("Number:"))
-    if N == 0:
-        check = False
-    else:
-        sm = sm + N             
-        n = n + 1
-sr = sm/n         # <----- СУММУ ЧИСЕЛ ПОДЕЛИТЬ НА КОЛИЧЕСТВО!
-print(sr)
+def func():
+    n = 0             # ЧИСЕЛ
+    sm = 0            # СУММА
+    sr = 0            # ОТВЕТ
+    check = True
+    while check:
+        N = int(input("Number:"))
+        if N == 0:
+            check = False
+        else:
+            sm = sm + N             
+            n = n + 1
+    sr = sm/n         # <----- СУММУ ЧИСЕЛ ПОДЕЛИТЬ НА КОЛИЧЕСТВО!
+    print(sr)
+func()
