3e87d59c611902babb4edc555cd6c7434fd24e38
BreadGitHub <113381962+BreadGitHub@users.noreply.github.com>
2022-11-26 20:24:41 +0300

    Update Task 1 (variant 3).py

--- Lesson 7/Task 1 (variant 3).py
+++ Lesson 7/Task 1 (variant 3).py
@@ -1,12 +1,14 @@
-n = int(input("Array length:"))
-N = [ ]
-sum = 0
-for i in range(n):
-    print("Enter the", i, "element")
-    N.append(int(input(i)))
-print("Array:", N)
-for i in range(n):
-    if N[i] % 2 != 0:
-        sum += N[i]
-        print("odd:", N[i])
-print("sum:", sum)
+def func():
+    n = int(input("Array length:"))
+    N = [ ]
+    sum = 0
+    for i in range(n):
+        print("Enter the", i, "element")
+        N.append(int(input(i)))
+    print("Array:", N)
+    for i in range(n):
+        if N[i] % 2 != 0:
+            sum += N[i]
+            print("odd:", N[i])
+    print("sum:", sum)
+func()
