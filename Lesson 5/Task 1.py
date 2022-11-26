375424c91cb9a87ebf4d0e598dea044700e45718
BreadGitHub <113381962+BreadGitHub@users.noreply.github.com>
2022-11-26 20:12:03 +0300

    Update Task 1.py

--- Lesson 5/Task 1.py
+++ Lesson 5/Task 1.py
@@ -1,5 +1,6 @@
-N = int(input("Number:"))
-n = 1  #   <---Он должен выводить это!
-while N >= n**2:
-    print(n**2)
-    n = n+1
+def func(N):
+    n = 1  #   <---Он должен выводить это!
+    while N >= n**2:
+        print(n**2)
+        n = n+1
+func(N = int(input("Number:")))
